8a14b39d8f52cf6cd3b3fde307c1ca655a1543f2
d.boldirev <wentorn@yadenx.ru>
2023-05-25 22:14:14 +0400

    fixes

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector('.popup');
 const nameInput = document.querySelector('.popup__input_name'); 
 const descInput = document.querySelector('.popup__input_description'); 
-const iconClose = document.querySelector('.popup__button-close'); 
+const iconClose = document.querySelector('.popup__button_close'); 
 const formElement = document.querySelector('.popup__form');
 const textName = document.querySelector('.profile__name'); 
 const textDesc = document.querySelector('.profile__description'); 
@@ -9,13 +9,13 @@ const editButton = document.querySelector('.profile__button-edit');
 
 
 function onPopup () {
-    popup.classList.add('popup__active');
+    popup.classList.add('popup_active');
     nameInput.value = textName.textContent;
     descInput.value = textDesc.textContent;
 }
 
 function closePopup () {
-    popup.classList.remove('popup__active');
+    popup.classList.remove('popup_active');
 }
 
 function formSubmitHandler (data) {
