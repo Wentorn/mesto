21685f4fe4de658415e399073fa7a0790eea078f
Wentorn <31894401+Wentorn@users.noreply.github.com>
2023-05-26 23:55:25 +0400

    review updates

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 const popup = document.querySelector('.popup');
 const nameInput = document.querySelector('.popup__input_name'); 
 const descInput = document.querySelector('.popup__input_description'); 
-const iconClose = document.querySelector('.popup__button_close'); 
+const iconClose = document.querySelector('.popup__button-close'); 
 const formElement = document.querySelector('.popup__form');
 const textName = document.querySelector('.profile__name'); 
 const textDesc = document.querySelector('.profile__description'); 
 const editButton = document.querySelector('.profile__button-edit'); 
-
+const iconLike = document.querySelector('.elements__button-like');
 
 function onPopup () {
     popup.classList.add('popup_active');
@@ -25,8 +25,15 @@ function formSubmitHandler (data) {
     closePopup();
 }
 
+function toggleLike () {
+    if (iconLike.classList.contains('elements__button-like_active')) {
+        iconLike.classList.remove("elements__button-like_active");
+    } else {
+        iconLike.classList.add("elements__button-like_active");
+    }
+  }
+
 formElement.addEventListener('submit', formSubmitHandler);
 iconClose.addEventListener('click', closePopup);
 editButton.addEventListener('click', onPopup);
-
-
+iconLike.addEventListener('click', toggleLike);
